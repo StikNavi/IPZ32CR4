534580c639a32851be60e4d54ee18ff38d2f2188
StikNavi <stiknavi@gmail.com>
2024-03-02 15:57:00 +0200

    Project12

--- app/src/main/java/ua/edu/lntu/cw4/MainActivity.kt
+++ app/src/main/java/ua/edu/lntu/cw4/MainActivity.kt
@@ -17,7 +17,6 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import ua.edu.lntu.cw4.R
 import ua.edu.lntu.cw4.ui.theme.IPZ32CR4Theme
 
 class MainActivity : ComponentActivity() {
