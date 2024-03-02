75b614d7171d4846269928055605e0d0aa5450a6
StikNavi <stiknavi@gmail.com>
2024-03-02 12:06:29 +0200

    Project1

--- app/src/main/java/ua/edu/lntu/cw4/MainActivity.kt
+++ app/src/main/java/ua/edu/lntu/cw4/MainActivity.kt
@@ -3,21 +3,77 @@ package ua.edu.lntu.cw4
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.compose.runtime.saveable.rememberSaveable
 import ua.edu.lntu.cw4.ui.theme.IPZ32CR4Theme
 
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            IPZ32CR4Theme {
+                Surface(color = MaterialTheme.colors.background) {
+                    MyApp()
+                }
+            }
+        }
+    }
+}
 
+@Composable
+fun MyApp() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "screen1") {
+        composable("screen1") { Screen1(navController = navController) }
+        composable("screen2/{itemNumber}") { backStackEntry ->
+            val itemNumber = backStackEntry.arguments?.getString("itemNumber") ?: ""
+            Screen2(itemNumber = itemNumber)
+        }
+    }
+}
+
+@Composable
+fun Screen1(navController: NavHostController) {
+    val items = remember { (1..10).toList() }
+    Column(modifier = Modifier.padding(16.dp)) {
+        Text(text = "Screen 1", style = MaterialTheme.typography.h5)
+        Spacer(modifier = Modifier.height(16.dp))
+        items.forEach { item ->
+            Text(
+                text = "Item $item",
+                modifier = Modifier.clickable {
+                    navController.navigate("screen2/$item")
+                }
+            )
+        }
+    }
+}
+
+@Composable
+fun Screen2(itemNumber: String) {
+    Column(modifier = Modifier.padding(16.dp)) {
+        Text(text = "Screen 2", style = MaterialTheme.typography.h5)
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(text = "Item Number: $itemNumber")
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun DefaultPreview() {
     IPZ32CR4Theme {
-        Greeting("Android")
+        MyApp()
     }
 }
