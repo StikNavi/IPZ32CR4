1e7c23e82b83912f8b262923d31e85a1068a76c9
StikNavi <stiknavi@gmail.com>
2024-03-02 12:56:25 +0200

    Project5

--- app/src/main/java/ua/edu/lntu/cw4/MainActivity.kt
+++ app/src/main/java/ua/edu/lntu/cw4/MainActivity.kt
@@ -3,47 +3,29 @@ package com.example.uaedulntucw4
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.CenterAlignedTopAppBar
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import ua.edu.lntu.cw4.R
 import ua.edu.lntu.cw4.ui.theme.IPZ32CR4Theme
 
 class MainActivity : ComponentActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             IPZ32CR4Theme {
-                LemonadeApp()
+                MedicineApp()
             }
         }
     }
@@ -51,27 +33,30 @@ class MainActivity : ComponentActivity() {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun LemonadeApp() {
-
+fun MedicineApp() {
     var currentStep by remember { mutableStateOf(1) }
-
-    var squeezeCount by remember { mutableStateOf(0) }
+    var selectedItem by remember { mutableStateOf<Int?>(null) }
 
     Scaffold(
         topBar = {
             CenterAlignedTopAppBar(
                 title = {
                     Text(
-                        text = "Lemonade",
+                        text = stringResource(R.string.app_name),
                         fontWeight = FontWeight.Bold
                     )
                 },
-                colors = TopAppBarDefaults.smallTopAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.primaryContainer,
-                    titleContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
-                    navigationIconContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
-                    actionIconContentColor = MaterialTheme.colorScheme.onPrimaryContainer
-                )
+                actions = {
+                    IconButton(
+                        onClick = { /* Handle action */ },
+                        content = {
+                            Icon(
+                                painter = painterResource(id = R.drawable.ic_settings),
+                                contentDescription = "Settings"
+                            )
+                        }
+                    )
+                }
             )
         }
     ) { innerPadding ->
@@ -83,99 +68,70 @@ fun LemonadeApp() {
             color = MaterialTheme.colorScheme.background
         ) {
             when (currentStep) {
-                1 -> {
-                    LemonTextAndImage(
-                        textLabelResourceId = R.string.lemon_select,
-                        drawableResourceId = R.drawable.lemon_tree,
-                        contentDescriptionResourceId = R.string.lemon_tree_content_description,
-                        onImageClick = {
-                            currentStep = 2
-                            squeezeCount = (2..4).random()
-                        }
-                    )
-                }
-                2 -> {
-                    LemonTextAndImage(
-                        textLabelResourceId = R.string.lemon_squeeze,
-                        drawableResourceId = R.drawable.lemon_squeeze,
-                        contentDescriptionResourceId = R.string.lemon_content_description,
-                        onImageClick = {
-                            squeezeCount--
-                            if (squeezeCount == 0) {
-                                currentStep = 3
-                            }
-                        }
-                    )
-                }
-
-                3 -> {
-                    LemonTextAndImage(
-                        textLabelResourceId = R.string.lemon_drink,
-                        drawableResourceId = R.drawable.lemon_drink,
-                        contentDescriptionResourceId = R.string.lemonade_content_description,
-                        onImageClick = {
-                            currentStep = 4
-                        }
-                    )
-                }
-                4 -> {
-                    LemonTextAndImage(
-                        textLabelResourceId = R.string.lemon_empty_glass,
-                        drawableResourceId = R.drawable.lemon_restart,
-                        contentDescriptionResourceId = R.string.empty_glass_content_description,
-                        onImageClick = {
-                            currentStep = 1
-                        }
-                    )
-                }
+                1 -> Screen1(
+                    onItemClick = { selectedItem = it + 1 },
+                    modifier = Modifier.fillMaxSize()
+                )
+                2 -> Screen2(
+                    selectedItemId = selectedItem,
+                    onBackClick = { currentStep = 1 },
+                    modifier = Modifier.fillMaxSize()
+                )
             }
         }
     }
 }
 
 @Composable
-fun LemonTextAndImage(
-    textLabelResourceId: Int,
-    drawableResourceId: Int,
-    contentDescriptionResourceId: Int,
-    onImageClick: () -> Unit,
-    modifier: Modifier = Modifier
-) {
-    Box(
+fun Screen1(onItemClick: (Int) -> Unit, modifier: Modifier = Modifier) {
+    val list = (1..10).toList() // Довільний список елементів
+    LazyColumn(
         modifier = modifier
+    ) {
+        items(list) { item ->
+            ListItem(
+                text = { Text("Item $item") },
+                icon = {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                        contentDescription = "Item Icon"
+                    )
+                },
+                modifier = Modifier.clickable { onItemClick(item) }
+            )
+        }
+    }
+}
+
+@Composable
+fun Screen2(selectedItemId: Int?, onBackClick: () -> Unit, modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier,
+        contentAlignment = Alignment.Center
     ) {
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center,
-            modifier = Modifier.fillMaxSize()
+            modifier = Modifier.padding(16.dp)
         ) {
-            Button(
-                onClick = onImageClick,
-                shape = RoundedCornerShape(dimensionResource(R.dimen.button_corner_radius)),
-                colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.tertiaryContainer)
-            ) {
-                Image(
-                    painter = painterResource(drawableResourceId),
-                    contentDescription = stringResource(contentDescriptionResourceId),
-                    modifier = Modifier
-                        .width(dimensionResource(R.dimen.button_image_width))
-                        .height(dimensionResource(R.dimen.button_image_height))
-                        .padding(dimensionResource(R.dimen.button_interior_padding))
-                )
-            }
-            Spacer(modifier = Modifier.height(dimensionResource(R.dimen.padding_vertical)))
             Text(
-                text = stringResource(textLabelResourceId),
-                style = MaterialTheme.typography.bodyLarge
+                text = "Selected Item: ${selectedItemId ?: "None"}",
+                style = MaterialTheme.typography.h5
             )
+            Spacer(modifier = Modifier.height(16.dp))
+            Button(
+                onClick = onBackClick,
+                colors = ButtonDefaults.buttonColors(backgroundColor = Color.Red)
+            ) {
+                Text(text = "Back")
+            }
         }
     }
 }
 
 @Preview
 @Composable
-fun LemonPreview() {
-    IPZ32CR4Theme() {
-        LemonadeApp()
+fun MedicinePreview() {
+    IPZ32CR4Theme {
+        MedicineApp()
     }
 }
